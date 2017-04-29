232b437d77cbb2a3c38371c41f6d4a86f1efa7c5
Scott van Looy <scott@quul.net>
2017-04-29 23:42:12 +0200

    Template data abstraction

---
+++ data/templatedata.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = {
+    index: {
+        title: 'The Test Page',
+        pageTitle: 'Welcome!'
+    },
+    error: {
+        message: 'Oops',
+        error: {
+            status: '404',
+            stack: 'mistakes'
+        }
+    }
+};
