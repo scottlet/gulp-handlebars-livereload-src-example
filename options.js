67d95f5bc0699adf002a6afc302db6cd3f9624cd
Scott van Looy <scott@quul.net>
2019-10-21 08:13:50 +0200

    Update to options

--- options.js
+++ options.js
@@ -1,5 +1,8 @@
-'use strict';
-
-const OPTIONS = {};
+/* Use this to override any of the options in CONSTS.js inside the builder repo. Should probably not be necessary
+except for name and version */
+const OPTIONS = {
+    NAME: 'Handlebars Source', // change to name of your project
+    VERSION: '0.0.2' // increment with each release
+};
 
 module.exports = OPTIONS;
